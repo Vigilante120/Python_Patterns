35ea2eb174dea6b2e503b65b043809cfc073f87e
Vigilante120 <79633096+Vigilante120@users.noreply.github.com>
2025-03-06 22:14:46 +0530

    Added cube.py file

---
+++ cube.py
@@ -0,0 +1,19 @@
+n= 6
+
+for i in range(1, n + 1):
+    print(f"Current Number is {i} and the cube is {i * i * i}")
+
+"""
+Variable Declaration:
+n = 6: Defines the upper limit of the range of numbers to be processed.
+
+For Loop:
+for i in range(1, n + 1):: Iterates through numbers from 1 to n (inclusive).
+
+Print Statement:
+print(f"Current Number is {i} and the cube is {i * i * i}"):
+
+Uses an f-string to format the output.
+
+Displays the current number (i) and its cube (i * i * i).
+"""
