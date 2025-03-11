bfb9909b491134605059af9c55b446addb942a50
Nishant Bhandari <79633096+Vigilante120@users.noreply.github.com>
2025-03-11 10:42:38 +0530

    bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,8 @@
 unsorted = [30, 25, 55, 95, 5]
 
-
-for i in range(len(unsorted) -  1):
-    if unsorted[0] < unsorted[1]:
-        unsorted[0] = unsorted[1]
+for i in range(len(unsorted)):
+    for j in range(len(unsorted) - i - 1):
+        if unsorted[j] > unsorted[j + 1]:
+            unsorted[j], unsorted[j + 1] = unsorted[j + 1], unsorted[j]
 
 print(unsorted)
