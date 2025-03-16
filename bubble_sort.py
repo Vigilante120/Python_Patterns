01893c19f6e5ec7e6452946b30fb24b773300b19
Vigilante120 <79633096+Vigilante120@users.noreply.github.com>
2025-03-16 17:18:22 +0530

    bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,31 @@
-unsorted = [30, 25, 55, 95, 5]
+def bubble(n):
+    swapped = False
+    while not swapped:
+        swapped = True
+        for i in range(0, len(n)-1):
+            if n[i] > n[i+1]:
+                swapped = False
+                n[i], n[i + 1] = n[i+1], n[i]
+    return n
 
 
-for i in range(len(unsorted) -  1):
-    if unsorted[0] < unsorted[1]:
-        unsorted[0] = unsorted[1]
+unsorted = [5,4,3,2,1]
 
-print(unsorted)
+print(bubble(unsorted))
+
+"""
+Input: You pass an unsorted list (unsorted =) to the bubble() function.
+
+Initialization:
+A variable swapped is set to False initially. This variable is meant to track whether any swaps happen during a pass through the list.
+
+Outer Loop:
+The while not swapped: loop runs until no swaps are made in a complete pass through the list.
+Inside this loop, swapped is set to True at the beginning of each pass.
+
+Inner Loop:
+The for loop iterates over the indices of the list (from 0 to len(n)-2) and compares each pair of adjacent elements (n[i] and n[i+1]).
+If the first element in the pair (n[i]) is greater than the second (n[i+1]), they are swapped.
+
+Return: Once the list is sorted, it exits the loop and returns the sorted list.
+"""
