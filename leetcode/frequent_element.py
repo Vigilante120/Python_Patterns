40dda3859480098d2c86dba08a9b4924f1dad8d8
Nishant Bhandari <nishantbhandari@Nishants-MacBook-Air.local>
2025-08-27 21:07:14 +0530

    Sorted the files in there designated folders

---
+++ leetcode/frequent_element.py
@@ -0,0 +1,13 @@
+# nums = [3, 2, 3]
+# counts = {}
+#
+# for num in nums:
+#     counts[num] = counts.get(num, 0) + 1
+#
+# print(counts)
+#
+#
+students = {'name': 'nishant', 'age': 25, 'course': ['Math', 'CompSci']}
+
+students.update({'name': 'Nishant'})
+print(students)
