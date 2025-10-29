aa1ee6931b9f004a242f6274fc8318466a013172
Vigilante120 <bhandarinishant32@gmail.com>
2025-10-29 10:17:47 +0530

    Introduced Doubly Linked List Did 3 Actions appending, printing and making the structure of the linked list

---
+++ doubly_linked_list/dllIntro.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value=None):
+        if value is not None:
+            new_node = Node(value)
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            self.head = None
+            self.tail = None
+            self.length = 0
+    
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+my_dll = DoublyLinkedList(7)
+my_dll.append(20)
+my_dll.print_list()
