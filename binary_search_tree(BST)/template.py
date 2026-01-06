820d83a2d6f3828d5cf5a7309ea0367428305045
Vigilante120 <bhandarinishant32@gmail.com>
2026-01-06 20:28:38 +0530

    added insert function for BST(binary search tree)

--- binary_search_tree(BST)/template.py
+++ binary_search_tree(BST)/template.py
@@ -12,5 +12,37 @@ class BinarySearchTree:
         self.root = None # BST FIRST u dont add the root value at the time u create the class instead we will use insert function to initialize the code
         # the perfect constructor
 
+    def insert(self, value):
+        #TC: if root = None, root = new_node ;) 
+        new_node = Node(value)
+        if self.root == None:
+            self.root = new_node
+            return True # we will break out here as the root is freshly produced
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value: # we are comparing the values here so we are using(.value)
+                return False
+            
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+            
+
+
 my_tree = BinarySearchTree()
-print(my_tree.root) # this will print None :) 
+print(my_tree.insert(2))
+print(my_tree.insert(1))
+print(my_tree.insert(3))
+
+print(my_tree.root.value)
+print(my_tree.root.left.value)
+print(my_tree.root.right.value)
