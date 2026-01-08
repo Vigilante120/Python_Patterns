d93ac5af241bcb974a8a9aa1e6c9c7f5fda3f925
Vigilante120 <bhandarinishant32@gmail.com>
2026-01-08 23:50:32 +0530

    added hashtables theory and constructor

--- hashtables/intro.py
+++ hashtables/intro.py
@@ -1,3 +1,17 @@
 # dictionary Key:value Pair 
 # set_item(key, value)
 
+# hash will be added to nails, nuts, screws
+# collision happens when 2 vals are entered inside the same address
+
+# find empty spot then store the data inside this that is called linear probing (open addressing) where u dont have more than 1 key:val pair at any address #0x
+
+# seperate chaining is storing all the address like having linked list and say we have multiple vals we iterate through it to find the value we need this is the way to deal with collisons !!!this is imp to understand hashtables
+
+# always have a prime numbers of address 0 - 7
+
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None] * size # create a list of 7 items in it 
+
+
