ddc23677104746897cd36a97de0d36d1a6464e9a
Vigilante120 <bhandarinishant32@gmail.com>
2026-01-09 01:03:48 +0530

    added the whole constructor of the hashtable

--- hashtables/intro.py
+++ hashtables/intro.py
@@ -14,4 +14,15 @@ class HashTable:
     def __init__(self, size=7):
         self.data_map = [None] * size # create a list of 7 items in it 
 
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+    
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
 
+my_hash_table = HashTable()
+my_hash_table.print_table()
