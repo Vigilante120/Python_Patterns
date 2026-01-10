14cd097b7da5ca93b5382baf3882aca7b513b2e5
Vigilante120 <bhandarinishant32@gmail.com>
2026-01-10 22:44:55 +0530

    added get_item function inside the HashTable/intro.py

--- hashtables/intro.py
+++ hashtables/intro.py
@@ -31,6 +31,19 @@ class HashTable:
         self.data_map[index].append([key, value])
 
 
+# Time to create a get function here so we can access the values using keys :) 
+
+    def get(self, key): # we hit the key and get the value
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+
+
+
+# EXAMPLE USAGE
 my_hash_table = HashTable()
 
 my_hash_table.set_item('bolts', 1400)
