4446d6d4bb79d795cb368fd9950036daa724d037
Vigilante120 <bhandarinishant32@gmail.com>
2026-01-11 19:22:25 +0530

    Added a problem Find_duplicate using hashtables

---
+++ hashtables/find_duplicate.py
@@ -0,0 +1,20 @@
+
+def find_duplicates(nums):
+    counts = {}
+    for num in nums:
+        counts[num] = counts.get(num, 0) + 1
+    
+    duplicates = []
+    for num, count in counts.items():
+        if count > 1:
+            duplicates.append(num)
+    return duplicates
+    
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
