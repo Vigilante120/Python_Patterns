c25ae73934aa747b9c7095df8e9f9982d534e83d
Vigilante120 <bhandarinishant32@gmail.com>
2026-01-15 21:58:37 +0530

    added graphs intro and added 2 functions -> print_graph() & add_vertex()

---
+++ graphs/graphs_intro.py
@@ -0,0 +1,21 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+        # this just create a dict 
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex])  
+              
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            # no duplicate vertex
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+my_graph = Graph()
+
+my_graph.add_vertex('A')
+
+my_graph.print_graph()
