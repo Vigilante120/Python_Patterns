de0be42a6f37dc88025ab08ea90fcb971fd5feb5
Vigilante120 <bhandarinishant32@gmail.com>
2026-01-28 22:43:57 +0530

    added heap folder for data structure and algorithms

---
+++ heaps/intro.py
@@ -0,0 +1,5 @@
+# it is a complete tree you fill it from left to right on each level with no gap then we go to next level then we fill it again from left to right 
+
+# in heap we skip index 0 it keeps the calculation of locating the parents or child nodes easy
+
+
