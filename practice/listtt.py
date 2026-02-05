6d1d4c79579328219b3976ef1176aa113f0e705c
Vigilante120 <bhandarinishant32@gmail.com>
2026-02-05 21:37:38 +0530

    interview questions added in practice dir

---
+++ practice/listtt.py
@@ -0,0 +1,95 @@
+from collections import Counter
+
+movie_data = [
+    ["amit", "interstellar"],
+    ["vikas", "dhurandar"],
+    ["amit", "mafia"],
+    ["priya", "pie"],
+    ["vikas", "mafia"]
+]
+
+# unique names done
+unique_name = []
+for name, movie in movie_data:
+    if name not in unique_name:
+        unique_name.append(name)
+print(unique_name)
+
+# count unique movies
+unique_movies = []
+for name, movie in movie_data:
+    if movie not in unique_movies:
+        unique_movies.append(movie)
+print(len(unique_movies))
+
+# movies occurence
+movie_count = {}
+for name, movie in movie_data:
+    if movie not in movie_count:
+        movie_count[movie] = 1 
+    else:
+        movie_count[movie] += 1
+
+# expected output is
+"""
+expected op is 
+dhurandar 1
+interstellar 1
+mafia 1
+pie 2 
+"""
+for movie, count in movie_count.items():
+    print(f"{movie} {count}")
+
+
+movies_only = [movie for name, movie in movie_data]
+
+# Count them automatically
+counts = Counter(movies_only)
+
+print(counts)
+
+
+election_data = [
+    # [Precinct, Position, Name, Points]
+    [1, "Mayor",      "Amit",  500],
+    [1, "Counsellor", "Priya", 200],
+    [1, "Counsellor", "Rohan", 150],
+
+    [2, "Mayor",      "Amit",  450],
+    [2, "Counsellor", "Rohan", 300],
+
+    [3, "Mayor",      "Priya", 600],
+    [3, "Counsellor", "Amit",  120],
+
+    [4, "Mayor",      "Rohan", 400],
+    [4, "Counsellor", "Priya", 250],
+    [4, "Counsellor", "Amit",  100]
+]
+
+amit_score = 0
+for precinct, position, name, points in election_data:
+    if name == "Amit":
+        print(f"Amit got {points} points in precinct {precinct}")
+        amit_score += points
+print(amit_score)
+
+mayor_scores = {}
+consellor_scores = {}
+
+for precinct, position, name, points in election_data:
+    if position == "Mayor":
+        if name not in mayor_scores:
+            mayor_scores[name] = 0
+        mayor_scores[name] += points
+    elif position == "Counsellor":
+        if name not in consellor_scores:
+            consellor_scores[name] = 0
+        consellor_scores[name] += points
+
+winner_mayor = max(mayor_scores.items(), key=lambda x:x[1])
+winner_counsellor = max(consellor_scores.items(), key=lambda x: x[0])
+print(mayor_scores)
+print("Final Results")
+print(f"Winning Mayor: {winner_mayor[0]} with {winner_mayor[1]} total Points")
+print(f"Winning Counsellor: {winner_counsellor[0]} with {winner_counsellor[1]} total points")
