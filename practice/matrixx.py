7e88d194e676207b1f823639d7588859d5b38dd7
Vigilante120 <bhandarinishant32@gmail.com>
2026-02-06 13:23:07 +0530

    added 2d tuples and some more traversal techniques

--- practice/matrixx.py
+++ practice/matrixx.py
@@ -30,9 +30,27 @@ meats =      ["chicken", "fish", "turkey"]
 groceries = [fruits, vegetables, meats]
 print(groceries)
 
-print(groceries[1])
+print(groceries[1][1])
 
+for row in groceries:
+    for food in row:
+        print(food, end=" ")
+    print()
 
 # for row in groceries:
 #     for element in row:
 #         print(element, end=" ")
+
+print("New Exercises")
+# tuple is faster than list
+
+num_pad = ([1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9],
+           ["*", 0, "#"])
+
+
+for row in num_pad:
+    for num in row:
+        print(num, end= " ")
+    print()
