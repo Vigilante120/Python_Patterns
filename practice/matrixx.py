399b9e8ca774252cf153845b9ec32a1cf51e1c0b
Vigilante120 <bhandarinishant32@gmail.com>
2026-02-06 14:22:53 +0530

    added lots of problems which included reverse function in loop , list and int

--- practice/matrixx.py
+++ practice/matrixx.py
@@ -54,3 +54,102 @@ for row in num_pad:
     for num in row:
         print(num, end= " ")
     print()
+print("Practice time")
+
+def spirtal_matrix(matrix):
+    if not matrix or not matrix[0]:
+        return []
+    
+    res = []
+    top, bottom = 0, len(matrix) - 1
+    left, right = 0, len(matrix[0]) - 1
+
+    while top <= bottom and left <= right:
+        # dir 1
+        for col in range(left, right + 1):
+            res.append(matrix[top][col])
+        top += 1
+        # dir 2
+        for row in range(top, bottom + 1):
+            res.append(matrix[row][right])
+        right -= 1
+        # dir 3 move left along bottom row
+        if top <= bottom:
+            for col in range(right, left - 1, -1):
+                res.append(matrix[bottom][col])
+            bottom -= 1
+        # dir 4
+        if left <= right:
+            for row in range(bottom, top - 1, -1):
+                res.append(matrix[row][left])
+            left += 1
+            
+        
+        return res
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(spirtal_matrix(matrix))
+
+print("looping in reverse")
+
+# range(start, stop, step)
+for i in range(10, 0, -1):
+    print(i)
+
+# to reverse list
+print("reverse a list using reverse method")
+nums = [4,3,2,1]
+for i in reversed(nums):
+    print(i, end= " ")
+
+print("reverse through index")
+# reverse through index
+
+nums = [10,20,30,40]
+for i in range(len(nums) - 1, - 1, -1):
+    print(nums[i])
+
+# reverse string s
+
+s = "python"
+for char in range(len(s) -1, -1, -1):
+    print(s[char], end="")
+print()
+
+s = "I love coding"
+for char in range(len(s)- 1, -1 , -1):
+    print(s[char], end="")
+
+print("\nprinting even numbers in reverse")
+
+n = 10 
+for i in range(n, 0, -1):
+    if i % 2 == 0:
+        print(i)
+
+print("\nReverse index")
+
+arr = [5, 15, 25]
+
+for i in range(len(arr) -1 , -1, -1):
+    print(f"Index: {i} Value: {arr[i]}")
+
+# reverse pattern 
+
+print("Reverse Pattern")
+n = 4 
+for i in range(n, -1, -1):
+    print("*" * i)
+
+
+n = int(input("Enter a number: "))
+
+while n > 0:
+    r = n % 10
+    n = n //10
+    print(r, end="",)
