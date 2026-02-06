17edf1dc62d08db184653d7ea3da4c9d8b562386
Vigilante120 <bhandarinishant32@gmail.com>
2026-02-06 13:04:26 +0530

    added more practice on the matrix part (unrusting my basics)

---
+++ practice/matrixx.py
@@ -0,0 +1,38 @@
+# 2d Lists
+matrix = [[1,2,3], [4,5,6], [7,8,9]]
+
+rows, cols = 3,4 
+
+# matrix = [[2 for j in range(cols) for i in range(rows)]]
+# print(matrix)
+
+# using indices 
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        print(matrix[i][j], end=' ')
+    print()
+
+# direct iteration
+for row in matrix:
+    print(end='\n')
+    for element in row:
+        print(element, end=' ')
+print()
+
+
+
+#           --COLUMNS--
+fruits =     ["apple", "orange", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+#.           <--------------ROW----------->
+meats =      ["chicken", "fish", "turkey"]
+
+groceries = [fruits, vegetables, meats]
+print(groceries)
+
+print(groceries[1])
+
+
+# for row in groceries:
+#     for element in row:
+#         print(element, end=" ")
