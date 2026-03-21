378394736b2fc2b265b02913b1c973c1489170f7
Vigilante120 <bhandarinishant32@gmail.com>
2026-03-22 00:05:19 +0530

    added drop_item func

--- interview_prep /game.py
+++ interview_prep /game.py
@@ -13,8 +13,20 @@ def loot_item(item_name):
     else:
         print(f"There is no {clean_name} here to pick up")
 
+def drop_item(item_name):
+    clean_name = item_name.lower()
+    if clean_name in inventory:
+        inventory.remove(clean_name)
+        print(f"you dropped a {clean_name}")
+    else:
+        print(f"there is no {item_name}")
 
 item = input("Enter the item name: ")
 loot_item(item)
 
 print("Current Inventory: ", inventory)
+
+item = input("Enter the item name you want to drop: ")
+drop_item(item)
+
+print("Dropped item: ", item)
